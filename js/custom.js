9e5002ddf86ce693b748cf15e54d77acb1464bed
Jordy Webb <jordywebb@Jordys-MacBook-Pro.local>
2017-06-24 07:16:16 -0700

    add scrolling animation

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $('.slide-section').click(function(e) {
+    console.log('clicked');
+
+    var linkRef = $(this).attr('href');
+      $('html, body').animate({
+      scrollTop: $(linkRef).offset().top
+    });
+
+    e.preventDefault();
+  });
+});
